e324e2d8a1e3734637adb511e63a318948be48ca
John Deck <jdeck88@gmail.com>
2018-06-04 14:15:00 -0400

    changing restart options to be compliant with most current version of node

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pm2 restart download_proxy --max-memory-restart 300M 
-pm2 restart download_proxy_v2 --max-memory-restart 1000M --node-args="--max_old_space_size=1000M" 
-pm2 restart es_proxy --max-memory-restart 300M --node-args="--max_old_space_size=300M" 
+pm2 restart download_proxy_v2 --max-memory-restart 1000M --node-args="--max_old_space_size=1000" 
+pm2 restart es_proxy --max-memory-restart 300M --node-args="--max_old_space_size=300" 
 pm2 restart kibana_proxy --max-memory-restart 300M
-pm2 restart ontology_proxy --max-memory-restart 1000M --node-args="--max_old_space_size=1000M" 
-pm2 restart ontology_proxy_v2  --max-memory-restart 300M --node-args="--max_old_space_size=1000M" 
+pm2 restart ontology_proxy --max-memory-restart 1000M --node-args="--max_old_space_size=1000" 
+pm2 restart ontology_proxy_v2  --max-memory-restart 300M --node-args="--max_old_space_size=1000" 
