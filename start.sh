194c2fc86e87f66095b2313724e5dc448bcc812a
jdeck88 <jdeck88@gmail.com>
2020-11-18 16:46:04 -0500

    adding futres download function

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pm2 start download_proxy_v2.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
+pm2 start download_futres_proxy_v2.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
 pm2 start es_proxy.js --max-memory-restart 300M --node-args="--max_old_space_size=300"  --log-date-format="YYYY-MM-DD HH:mm Z"
 pm2 start ontology_proxy.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000"  --log-date-format="YYYY-MM-DD HH:mm Z"
 pm2 start ontology_proxy_v2.js  --max-memory-restart 300M --node-args="--max_old_space_size=1000"  --log-date-format="YYYY-MM-DD HH:mm Z"
