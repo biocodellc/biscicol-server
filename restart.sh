e7d464b948eae34b0d5998d68e54d53e82bd164b
John Deck <jdeck88@gmail.com>
2021-08-10 21:22:55 +0000

    modified start scripts

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ pm2 restart api.v1.ppo.js  --max-memory-restart 300M --node-args="--max_old_spac
 pm2 restart api.v2.ppo.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000"  --log-date-format="YYYY-MM-DD HH:mm Z"
 pm2 restart api.v2.download.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
 pm2 restart api.v3.download.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
-pm2 restart futresapi.v1.query.js --max-memory-restart 300M --node-args="--max_old_space_size=300"  --log-date-format="YYYY-MM-DD HH:mm Z"
 pm2 restart futresapi.v2.fovt.js  --max-memory-restart 300M --node-args="--max_old_space_size=1000"  --log-date-format="YYYY-MM-DD HH:mm Z"
 pm2 restart futresapi.v2.download.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
 pm2 restart futresapi.v3.download.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
+pm2 restart adapi.v3.download.js --max-memory-restart 1000M --node-args="--max_old_space_size=1000" --log-date-format="YYYY-MM-DD HH:mm Z" 
