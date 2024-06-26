55725646167444b9043b721edb14d0ef81f7f831
jdeck88 <jdeck88@gmail.com>
2024-06-26 12:29:44 +0000

    updating futres api script to point to new location

--- futres.api.v1.query.js
+++ futres.api.v1.query.js
@@ -28,7 +28,7 @@ app.use(cors({origin: '*'}), function(req, res, body) {
 		}
 
 		// The incoming requesting string now contains a reference that should be removed
-		req.url = req.url.replace('api/v1/query/','')
+		req.url = req.url.replace('futres/api/v1/query/','')
 		// pass the request to elasticsearch
         var url = apiServerHost + req.url;
         console.log(url)
